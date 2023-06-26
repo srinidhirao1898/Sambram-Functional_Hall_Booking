95eb682b84b831cbdb1ac987c5f0597ffddf0c9a
srinidhirao1898 <136192393+srinidhirao1898@users.noreply.github.com>
2023-06-26 23:19:11 +0530

    Add files via upload

---
+++ sambram/SambramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.sambram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SambramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
